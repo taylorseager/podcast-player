97e8cd75157cab6140e29318655c5724fa5e2df5
Taylor Seager <taylorseager718@gmail.com>
2024-04-27 09:28:03 -0500

    save point - propTypes added and bootstrap card code copied over

--- components/PlaylistCard.js
+++ components/PlaylistCard.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import PropTypes from 'prop-types';
 
-export default async function PlaylistCard() {
+function PlaylistCard({ playlistObj }) {
   return (
-    <div>
-      This is a playlist card
-    </div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={playlistObj.image} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the content.
+        </Card.Text>
+        <Button variant="primary" className="m-2">VIEW</Button>
+        <Button variant="info">EDIT</Button>
+        <Button variant="danger" className="m-2">DELETE</Button>
+      </Card.Body>
+    </Card>
   );
 }
+
+PlaylistCard.propTypes = {
+  playlistObj: PropTypes.shape({
+    title: PropTypes.string,
+    podcastQuantity: PropTypes.string,
+    image: PropTypes.string,
+    ownerID: PropTypes.string,
+  }).isRequired,
+};
+
+export default PlaylistCard;
