a6d003f56aad76ceefa95fe361aac2f2473d1fe5
Lucas Martin <lucasmartin11990@gmail.com>
2024-05-04 10:24:36 -0500

    adding an extra line bc coding stuff

--- components/PodcastCard.js
+++ components/PodcastCard.js
@@ -1,8 +1,18 @@
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
+import { viewPlaylistDetails } from '../api/mergedData';
 
 export default function PodcastCard({ podcastObj }) {
+  const [playlistDetails, setPlaylistDetails] = useState({});
+  const router = useRouter();
+  const { id } = router.query;
+  useEffect(() => {
+    viewPlaylistDetails(id).then(setPlaylistDetails);
+  }, [id]);
+
   return (
     <Card className="podcastCard" style={{ width: '18rem' }}>
       <Card.Title>{podcastObj.name}</Card.Title>
@@ -17,12 +27,14 @@ export default function PodcastCard({ podcastObj }) {
         <Card.Text>
           Length: {podcastObj.length} Minutes
         </Card.Text>
-        <Link passHref href={`podcast/${podcastObj.id}`}>
+        <Link passHref href={`/podcast/${podcastObj.id}`}>
           <Button variant="primary">Add to Playlist</Button>
         </Link>
-        <Link href={`/podcast/${podcastObj.id}`} passHref>
-          <Button variant="primary" className="m-2">View</Button>
-        </Link>
+        {playlistDetails.title
+          ? (
+            <Button variant="primary" className="m-2">Remove</Button>
+          )
+          : ''}
       </Card.Body>
     </Card>
   );
